7a5f57d9dabae99db86ee9667de7a0f9a78d600b
Vitali Skarbinski <sfc-ruper@mail.ru>
2022-11-01 18:57:10 +0300

    try to add touch events for mobile frame

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,13 +7,55 @@ const Main = (props) => {
   // const setNewSection = () => {
   //   showContentSection = props.section
   // }
-
-
   //setNewSection(showContentSection)
+
+  let xDown = null;
+  let yDown = null;
+
+  function handleTouchStart(e) {
+    xDown = e.touches[0].clientX;
+    yDown = e.touches[0].clientY;
+  };
+
+  function handleTouchMove(e) {
+    if (!xDown || !yDown) {
+      return;
+    }
+
+    let xUp = e.touches[0].clientX;
+    let yUp = e.touches[0].clientY;
+
+    let xDiff = xDown - xUp;
+    let yDiff = yDown - yUp;
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+      if (xDiff > 0) {
+        /* left swipe */
+        showContentSection--
+        if (showContentSection < 0) {
+          showContentSection = props.content.length - 1
+        }
+        console.log(showContentSection);
+      } else {
+        /* right swipe */
+        showContentSection++
+        if (showContentSection === props.content.length) {
+          showContentSection = 0
+        }
+        console.log(showContentSection);
+      }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+  };
+
 
   return (
     <main className="main">
-      <div className="main-wrap">
+      <div className="main-wrap"
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        /* onClick={() => console.log(showContentSection)} */>
         {props.content[showContentSection].section}
       </div>
     </main>
