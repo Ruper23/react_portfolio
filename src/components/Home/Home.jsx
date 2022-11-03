065f497157fd76b07b924c1839ced8894dc52bfd
Vitali Skarbinski <sfc-ruper@mail.ru>
2022-11-03 22:40:42 +0300

    fixed swipe class

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,9 @@
 import './Home.scss'
 const Home = () => {
-  // const swipeAnimation = () => {
-  //   const home = document.querySelector('.home')
-  //   if (window.matchMedia("(max-width: 768px)")) {
-  //     home.classList.add('swipe')
-  //     setTimeout(() => { home.classList.remove('swipe') }, 3000)
-  //   }
-  // }
-  // swipeAnimation()
+  const frameSize = window.matchMedia("(min-width: 768px)").matches
 
   return (
-    <section className={window.matchMedia("(max-width: 768px)") ? 'home swipe' : 'home'}>
+    <section className={frameSize ? 'home' : 'home swipe'}>
       <div className="home__title">
         <h2 className="subtitle">Привет! Я,</h2>
         <h1 className="title">Скарбинский <br /> Виталий</h1>
